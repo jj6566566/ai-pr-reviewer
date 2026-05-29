4fdf601c1826e812d9a5f48353831aac732a95b9
jj <3304640934@qq.com>
2026-05-29 17:42:06 +0800

    fix: load_dotenv override + FERNET_KEY 生成

--- backend/config.py
+++ backend/config.py
@@ -3,7 +3,7 @@ import os
 import certifi
 from dotenv import load_dotenv
 
-load_dotenv()
+load_dotenv(override=True)
 
 os.environ.setdefault("SSL_CERT_FILE", certifi.where())
 os.environ.setdefault("REQUESTS_CA_BUNDLE", certifi.where())
